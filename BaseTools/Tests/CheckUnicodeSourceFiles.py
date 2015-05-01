f6953d7cf01bc8abb654cf4c83b87cefaa34e6b1
Jordan Justen <jordan.l.justen@intel.com>
2015-05-01 00:09:39 -0700

    BaseTools/Tests: Add unit test for AutoGen.UniClassObject
    This verifies that a UTF-16 data (with BOM) .uni file is successfully
    read.
    
    Cc: Yingke D Liu <yingke.d.liu@intel.com>
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>

---
+++ BaseTools/Tests/CheckUnicodeSourceFiles.py
@@ -0,0 +1,77 @@
+## @file
+#  Unit tests for AutoGen.UniClassObject
+#
+#  Copyright (c) 2015, Intel Corporation. All rights reserved.<BR>
+#
+#  This program and the accompanying materials
+#  are licensed and made available under the terms and conditions of the BSD License
+#  which accompanies this distribution.  The full text of the license may be found at
+#  http://opensource.org/licenses/bsd-license.php
+#
+#  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+#
+
+##
+# Import Modules
+#
+import os
+import unittest
+
+import codecs
+
+import TestTools
+
+from Common.Misc import PathClass
+import AutoGen.UniClassObject as BtUni
+
+from Common import EdkLogger
+EdkLogger.InitializeForUnitTest()
+
+class Tests(TestTools.BaseToolsTest):
+
+    SampleData = u'''
+        #langdef en-US "English"
+        #string STR_A #language en-US "STR_A for en-US"
+    '''
+
+    def EncodeToFile(self, encoding, string=None):
+        if string is None:
+            string = self.SampleData
+        data = codecs.encode(string, encoding)
+        path = 'input.uni'
+        self.WriteTmpFile(path, data)
+        return PathClass(self.GetTmpFilePath(path))
+
+    def UnicodeErrorFailure(self, encoding, shouldPass):
+        msg = 'UnicodeError should '
+        if shouldPass:
+            msg += 'not '
+        msg += 'be generated for '
+        msg += '%s data in a .uni file' % encoding
+        self.fail(msg)
+
+    def CheckFile(self, encoding, shouldPass, string=None):
+        path = self.EncodeToFile(encoding, string)
+        try:
+            BtUni.UniFileClassObject([path])
+            if shouldPass:
+                return
+        except UnicodeError:
+            if not shouldPass:
+                return
+            else:
+                self.UnicodeErrorFailure(encoding, shouldPass)
+        except Exception:
+            pass
+
+        self.UnicodeErrorFailure(encoding, shouldPass)
+
+    def testUtf16InUniFile(self):
+        self.CheckFile('utf_16', shouldPass=True)
+
+TheTestSuite = TestTools.MakeTheTestSuite(locals())
+
+if __name__ == '__main__':
+    allTests = TheTestSuite()
+    unittest.TextTestRunner().run(allTests)
